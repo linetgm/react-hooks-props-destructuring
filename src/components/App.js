6ced43660c1d2b0a632da7fd8d39c579098238e1
Linet <muthiigathoni@gmail.com>
2023-07-23 23:41:34 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,23 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
+
+function MovieCard({ title, posterSrc, genres }){
+  return (
+    <>
+   <div className="movie-card">
+      <img src={posterSrc} alt={title} />
+      <h2>{title}</h2>
+      <small>{genres.join(", ")}</small>
+    </div>
+    </>
+  )
+}
+
+
 
 export default App;
